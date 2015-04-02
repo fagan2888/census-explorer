29a98d9d82f005e2869f4e4f3e03bd929110a828
Jon Speiser <jspeis@gmail.com>
2015-04-02 12:57:29 -0400

    better naics attribute file, adding industry names to treemap

--- bls/cbp/run.py
+++ bls/cbp/run.py
@@ -6,8 +6,9 @@ api_key = os.environ.get('CENSUS_API_KEY')
 year = 2012
 state = "39"
 
-url = 'http://api.census.gov/data/{}/ewks?get=EMP,ESTAB,NAICS2012_TTL&in=state:{}&for=county:*&NAICS2012=*&key={}'
-target = url.format(year, state, api_key)
+#url = 'http://api.census.gov/data/{}/ewks?get=EMP,ESTAB,NAICS2012_TTL&in=state:{}&for=county:*&NAICS2012=*&key={}'
+#target = url.format(year, state, api_key)
+target='http://api.census.gov/data/2012/cbp?get=EMP,ESTAB,NAICS2012_TTL&for=county:*&in=state:39&NAICS2012=*'
 
 print "hitting", target
 response = requests.get(target)
@@ -15,16 +16,40 @@ data = json.loads( response.text )
 headers = data[0]
 results = data[1:]
 
+# '''
+# {'NAICS2012': '48-49', 'name': 'Transportation and Warehousing'}
+# {'NAICS2012': '31-33', 'name': 'Manufacturing'}
+# {'NAICS2012': '44-45', 'name': 'Retail Trade'}
+# '''
+
+def genL0(x):
+    if int(x) in [48, 49]:
+        return "48-49"
+    elif int(x) in [44, 45]:
+        return "44-45"
+    elif int(x) in [31, 32, 33]:
+        return "31-33"
+    else:
+        return x
+
 def to_json(arr):
     res = { headers[idx] : val for idx,val in enumerate(arr) }
     res["geo"] = int( state.zfill(2) + res["county"].zfill(3) )
     del res["county"]
     res["EMP"] = int(res["EMP"])
     res["ESTAB"] = int(res["ESTAB"])
+    naics = str(res["NAICS2012"])
+    res["l0"] = genL0(naics[:2])
+    res["l1"] = naics[:4]
     return res
 
-formatted_json = [ to_json(x) for x in results]
-
+formatted_json = [] # [ to_json(x) for x in results
+for x in results:
+    mycode =str(x[headers.index("NAICS2012")])
+    if "-" in mycode:
+        print mycode
+    if mycode != '00':
+        formatted_json.append(to_json(x))
 # group by county
 
 json.dump(formatted_json, open("output/cbp_{}.json".format(state), "w"))
--- naics/run.py
+++ naics/run.py
@@ -1,9 +1,16 @@
 import requests
 import json
 
-res = requests.get("http://api.naics.us/v0/q?year=2012")
-data = json.loads(res.text)
+#res = requests.get("http://api.naics.us/v0/q?year=2012")
+data = json.load(open("data/naics_2012.json"))
 
 formatted = [{x["title"] : str(x["code"])} for x in data]
+attrs = {}
 
-json.dump(formatted, open("output/naics.js", "w"))
+for x in data:
+    fullcode = str(x["code"])
+    title = str(x["title"])
+    attrs[fullcode] = {"name": title}
+
+#json.dump(formatted, open("output/naics.js", "w"))
+json.dump(attrs, open("output/attrs_naics.json", "w"))
